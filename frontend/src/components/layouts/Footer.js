2f235c94bec6415bff139e66777a26ae4b3a0eea
codesantaisai <jathurshan@protonmail.com>
2023-06-01 16:40:25 +0530

    home page ui changed 01.06.2023

--- frontend/src/components/layouts/Footer.js
+++ frontend/src/components/layouts/Footer.js
@@ -3,104 +3,83 @@ export  default function Footer (){
     return (
         <>
   {/* Footer Start */}
-  <div
-    className=" footer mt-5 pt-5 wow fadeIn"
-    data-wow-delay="0.1s"
-  >
-    <div className="container py-5">
-      <div className="row g-5">
-        <div className="col-lg-3 col-md-6">
-          <h1 className="fw-bold text-primary mb-4">
-            A<span className="text-secondary">uw</span>shi
-          </h1>
-          <p className="auwshi">
-          Auwshi, a leading herbal business, blends nature's wisdom with modern innovation. We craft premium herbal products that inspire well-being and vitality. Experience the transformative power of Auwshi and embrace a natural approach to health.
-          </p>
-          <div className="d-flex pt-2">
-            <a
-              className="btn btn-square btn-outline-light rounded-circle me-1"
-              href=""
-            >
-              <i className="fab fa-twitter" />
-            </a>
-            <a
-              className="btn btn-square btn-outline-light rounded-circle me-1"
-              href=""
-            >
-              <i className="fab fa-facebook-f" />
-            </a>
-            <a
-              className="btn btn-square btn-outline-light rounded-circle me-1"
-              href=""
-            >
-              <i className="fab fa-youtube" />
-            </a>
-            <a
-              className="btn btn-square btn-outline-light rounded-circle me-0"
-              href=""
-            >
-              <i className="fab fa-linkedin-in" />
-            </a>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 address">
-          <h4 className="text-dark mb-4 ">Address</h4>
-          <p>
-            <i className=" fa fa-map-marker-alt me-3" />
-          Chankanai, Vikneshwara road,No:09
-          </p>
-          <p>
-            <i className="fa fa-phone-alt me-3" />
-            +94 706443471
-          </p>
-          <p>
-            <i className="fa fa-envelope me-3" />
-            auwshiherbs@gmai
-          </p>
-        </div>
-        <div className="col-lg-3 col-md-6">
-          <h4 className="text-dark mb-4">Quick Links</h4>
-          <a className="btn btn-link" href="/aboutus">
-            About Us
+  <>
+  {/* Remove the container if you want to extend the Footer to full width. */}
+  <div>
+    <footer className="bg-dark text-center text-white">
+      {/* Grid container */}
+      <div className="container p-4 pb-0">
+        {/* Section: Social media */}
+        <section className="mb-4">
+          {/* Facebook */}
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-facebook-f" />
           </a>
-          {/* <a className="btn btn-link" href="">
-            Contact Us
-          </a> */}
-          <a className="btn btn-link" href="">
-            Our Services
+          {/* Twitter */}
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-twitter" />
           </a>
-          {/* <a className="btn btn-link" href="">
-            Terms &amp; Condition
+          {/* Google */}
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-google" />
           </a>
-          <a className="btn btn-link" href="">
-            Support
-          </a> */}
-        </div>
-        {/* <div className="col-lg-3 col-md-6">
-          <h4 className="text-light mb-4">Newsletter</h4>
-          <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-          <div className="position-relative mx-auto" style={{ maxWidth: 400 }}>
-            <input
-              className="form-control bg-transparent w-100 py-3 ps-4 pe-5"
-              type="text"
-              placeholder="Your email"
-            />
-           
-          </div>
-        </div> */}
+          {/* Instagram */}
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-instagram" />
+          </a>
+          {/* Linkedin */}
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-linkedin-in" />
+          </a>
+          {/* Github */}
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-github" />
+          </a>
+        </section>
+        {/* Section: Social media */}
       </div>
-    </div>
-    <div className="container-fluid copyright">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-            © <a href="#">Auwshi Herbs</a>, All Right Reserved.
-          </div>
-         
-        </div>
+      {/* Grid container */}
+      {/* Copyright */}
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2020 Copyright:
+        <a className="text-white" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
       </div>
-    </div>
+      {/* Copyright */}
+    </footer>
   </div>
+  {/* End of .container */}
+</>
+
+
   {/* Footer End */}
 </>
 
